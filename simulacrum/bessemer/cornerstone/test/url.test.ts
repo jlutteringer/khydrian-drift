78a73b27c728bc15a50e36ff32fa6cc62b38b505
John Lutteringer <jlutteringer@credera.com>
2025-01-01 01:45:18 -0600

    uncommenting tests

--- simulacrum/bessemer/cornerstone/test/url.test.ts
+++ simulacrum/bessemer/cornerstone/test/url.test.ts
@@ -1,138 +1,138 @@
 import { Urls } from '@bessemer/cornerstone'
 
 test('Urls.parse / Urls.build', () => {
-  // {
-  //   expect(Urls.parse('https://www.google.com')).toEqual(
-  //     Urls.build({
-  //       scheme: 'https',
-  //       host: 'www.google.com',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('https://www.google.com/')).toEqual(
-  //     Urls.build({
-  //       scheme: 'https',
-  //       host: 'www.google.com',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('www.google.com')).toEqual(
-  //     Urls.build({
-  //       host: 'www.google.com',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('www.google.com/')).toEqual(
-  //     Urls.build({
-  //       host: 'www.google.com',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('http://localhost:8080')).toEqual(
-  //     Urls.build({
-  //       scheme: 'http',
-  //       host: {
-  //         value: 'localhost',
-  //         port: 8080,
-  //       },
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('http://localhost:8080/')).toEqual(
-  //     Urls.build({
-  //       scheme: 'http',
-  //       host: 'localhost:8080',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('localhost:8080')).toEqual(
-  //     Urls.build({
-  //       host: {
-  //         value: 'localhost',
-  //         port: 8080,
-  //       },
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('localhost:8080/')).toEqual(
-  //     Urls.build({
-  //       host: {
-  //         value: 'localhost',
-  //         port: 8080,
-  //       },
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('https://john.lutteringer:password123@www.google.com')).toEqual(
-  //     Urls.build({
-  //       scheme: 'https',
-  //       host: 'www.google.com',
-  //       authentication: {
-  //         principal: 'john.lutteringer',
-  //         password: 'password123',
-  //       },
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('https://john.lutteringer:password123@www.google.com/')).toEqual(
-  //     Urls.build({
-  //       scheme: 'https',
-  //       host: 'www.google.com',
-  //       authentication: 'john.lutteringer:password123',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('https://john.lutteringer@www.google.com/')).toEqual(
-  //     Urls.build({
-  //       scheme: 'https',
-  //       host: 'www.google.com',
-  //       authentication: 'john.lutteringer',
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('john.lutteringer@www.google.com')).toEqual(
-  //     Urls.build({
-  //       host: 'www.google.com',
-  //       authentication: {
-  //         principal: 'john.lutteringer',
-  //       },
-  //     })
-  //   )
-  // }
-  //
-  // {
-  //   expect(Urls.parse('john.lutteringer:password123@www.google.com')).toEqual(
-  //     Urls.build({
-  //       host: 'www.google.com',
-  //       authentication: {
-  //         principal: 'john.lutteringer',
-  //         password: 'password123',
-  //       },
-  //     })
-  //   )
-  // }
+  {
+    expect(Urls.parse('https://www.google.com')).toEqual(
+      Urls.build({
+        scheme: 'https',
+        host: 'www.google.com',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('https://www.google.com/')).toEqual(
+      Urls.build({
+        scheme: 'https',
+        host: 'www.google.com',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('www.google.com')).toEqual(
+      Urls.build({
+        host: 'www.google.com',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('www.google.com/')).toEqual(
+      Urls.build({
+        host: 'www.google.com',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('http://localhost:8080')).toEqual(
+      Urls.build({
+        scheme: 'http',
+        host: {
+          value: 'localhost',
+          port: 8080,
+        },
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('http://localhost:8080/')).toEqual(
+      Urls.build({
+        scheme: 'http',
+        host: 'localhost:8080',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('localhost:8080')).toEqual(
+      Urls.build({
+        host: {
+          value: 'localhost',
+          port: 8080,
+        },
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('localhost:8080/')).toEqual(
+      Urls.build({
+        host: {
+          value: 'localhost',
+          port: 8080,
+        },
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('https://john.lutteringer:password123@www.google.com')).toEqual(
+      Urls.build({
+        scheme: 'https',
+        host: 'www.google.com',
+        authentication: {
+          principal: 'john.lutteringer',
+          password: 'password123',
+        },
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('https://john.lutteringer:password123@www.google.com/')).toEqual(
+      Urls.build({
+        scheme: 'https',
+        host: 'www.google.com',
+        authentication: 'john.lutteringer:password123',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('https://john.lutteringer@www.google.com/')).toEqual(
+      Urls.build({
+        scheme: 'https',
+        host: 'www.google.com',
+        authentication: 'john.lutteringer',
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('john.lutteringer@www.google.com')).toEqual(
+      Urls.build({
+        host: 'www.google.com',
+        authentication: {
+          principal: 'john.lutteringer',
+        },
+      })
+    )
+  }
+
+  {
+    expect(Urls.parse('john.lutteringer:password123@www.google.com')).toEqual(
+      Urls.build({
+        host: 'www.google.com',
+        authentication: {
+          principal: 'john.lutteringer',
+          password: 'password123',
+        },
+      })
+    )
+  }
 
   {
     expect(Urls.parse('www.google.com?q=Search%20Query')).toEqual(
