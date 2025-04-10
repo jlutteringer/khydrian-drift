3c51b10f43edfb4b12672cc8b7b5c021bf4927e1
John Lutteringer <john@branchv.com>
2025-04-10 14:19:53 -0500

    increment version

--- bessemer/cornerstone/src/expression/sql-expression-parser.ts
+++ bessemer/cornerstone/src/expression/sql-expression-parser.ts
@@ -5,13 +5,13 @@ import { BasicType, Dictionary } from '@bessemer/cornerstone/types'
 
 // JOHN need to add tests...
 type SqlFragment = string
-export type SqlExpressionMapperContext = {
+export type SqlExpressionParserContext = {
   variables: Dictionary<string>
   parameters: Dictionary<BasicType>
 }
-export const SqlExpressionMapper = new ExpressionMapper<SqlFragment, SqlExpressionMapperContext>()
+export const SqlExpressionParser = new ExpressionMapper<SqlFragment, SqlExpressionParserContext>()
 
-SqlExpressionMapper.register(Expressions.ValueExpression, (expression, _, context) => {
+SqlExpressionParser.register(Expressions.ValueExpression, (expression, _, context) => {
   const value = expression.value
   const parameterName = `_${Ulids.generate()}`
   if (!Objects.isBasic(value)) {
@@ -21,21 +21,21 @@ SqlExpressionMapper.register(Expressions.ValueExpression, (expression, _, contex
   context.parameters[parameterName] = value
   return `:${parameterName}`
 })
-SqlExpressionMapper.register(Expressions.VariableExpression, (expression, _, context) => {
+SqlExpressionParser.register(Expressions.VariableExpression, (expression, _, context) => {
   const variableName = context.variables[expression.name]
   Preconditions.isPresent(variableName, `Unable to resolve VariableExpression with name: ${expression.name}`)
   return variableName
 })
-SqlExpressionMapper.register(Expressions.NotExpression, (expression, map) => {
+SqlExpressionParser.register(Expressions.NotExpression, (expression, map) => {
   return `(NOT ${map(expression.value)})`
 })
-SqlExpressionMapper.register(Expressions.AndExpression, (expression, map) => {
+SqlExpressionParser.register(Expressions.AndExpression, (expression, map) => {
   return `(${expression.operands.map(map).join(' AND ')})`
 })
-SqlExpressionMapper.register(Expressions.OrExpression, (expression, map) => {
+SqlExpressionParser.register(Expressions.OrExpression, (expression, map) => {
   return `(${expression.operands.map(map).join(' OR ')})`
 })
-SqlExpressionMapper.register(Expressions.EqualsExpression, (expression, map) => {
+SqlExpressionParser.register(Expressions.EqualsExpression, (expression, map) => {
   const first = Arrays.first(expression.operands)!
   const rest = Arrays.rest(expression.operands)
 
@@ -48,15 +48,15 @@ SqlExpressionMapper.register(Expressions.EqualsExpression, (expression, map) => 
 //   expression.collection
 //   return `(someVariable IN (${expression.collection}))`
 // })
-SqlExpressionMapper.register(BasicExpressions.LessThanExpression, (expression, map) => {
+SqlExpressionParser.register(BasicExpressions.LessThanExpression, (expression, map) => {
   return `(${map(expression.left)} < ${map(expression.right)})`
 })
-SqlExpressionMapper.register(BasicExpressions.LessThanOrEqualExpression, (expression, map) => {
+SqlExpressionParser.register(BasicExpressions.LessThanOrEqualExpression, (expression, map) => {
   return `(${map(expression.left)} <= ${map(expression.right)})`
 })
-SqlExpressionMapper.register(BasicExpressions.GreaterThanExpression, (expression, map) => {
+SqlExpressionParser.register(BasicExpressions.GreaterThanExpression, (expression, map) => {
   return `(${map(expression.left)} > ${map(expression.right)})`
 })
-SqlExpressionMapper.register(BasicExpressions.GreaterThanOrEqualExpression, (expression, map) => {
+SqlExpressionParser.register(BasicExpressions.GreaterThanOrEqualExpression, (expression, map) => {
   return `(${map(expression.left)} >= ${map(expression.right)})`
 })
